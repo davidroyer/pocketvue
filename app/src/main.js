d92f827478e8f8142af0e11467b3e8477023b80d
David Royer <droyer01@gmail.com>
2016-10-09 12:39:49 -0400

    Initial commit

---
+++ app/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Electron from 'vue-electron'
+import Resource from 'vue-resource'
+
+Vue.use(Electron)
+Vue.use(Resource)
+Vue.config.debug = true
+
+import App from './App'
+
+/* eslint-disable no-new */
+new Vue({
+  ...App
+}).$mount('#app')
